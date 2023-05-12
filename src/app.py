0a18caaf51dc1bb6d74d663f1572bf1ce25f580e
Dertyos <juliansalcedo4@gmail.com>
2023-05-12 18:18:22 +0000

    agregadas clases y endpoints

--- src/app.py
+++ src/app.py
@@ -36,14 +36,176 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/users', methods=['GET'])
+def get_users():
+    # Get all users from the database.
+    with db.session as session:
+        users = session.query(User).all()
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    # Return a JSON response with the users.
+    return jsonify([user.serialize() for user in users]), 200
 
-    return jsonify(response_body), 200
+@app.route('/changes', methods=['POST'])
+def register_change():
+    # Get the user name, change type, and change data from the request body.
+    user_name = request.json['user_name']
+    change_type = request.json['change_type']
+    change_data = request.json['change_data']
+
+    # Insert a new row into the `changes` table.
+    with db.session as session:
+        change = Change(
+            timestamp=datetime.now(),
+            user_name=user_name,
+            change_type=change_type,
+            change_data=change_data,
+        )
+        session.add(change)
+        session.commit()
+
+    # Return a success message.
+    return jsonify({'message': 'Change registered successfully'}), 201
+
+
+
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    # Get the user with the specified ID from the database.
+    with db.session as session:
+        user = session.query(User).get(user_id)
+
+    # Return a JSON response with the user.
+    return jsonify(user.serialize()), 200
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    # Get the user data from the request body.
+    user_data = request.json
+
+    # Create a new user object.
+    user = User(
+        name=user_data['name'],
+        role=user_data['role'],
+        date_of_birth=user_data['date_of_birth'],
+        user_name=user_data['user_name'],
+        password=user_data['password'],
+    )
+
+    # Add the user to the database.
+    with db.session as session:
+        session.add(user)
+        session.commit()
+
+    # Return a success message.
+    return jsonify({'message': 'User created successfully'}), 201
+
+@app.route('/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    # Get the user data from the request body.
+    user_data = request.json
+
+    # Get the user with the specified ID from the database.
+    with db.session as session:
+        user = session.query(User).get(user_id)
+
+    # Update the user's data.
+    user.name = user_data['name']
+    user.role = user_data['role']
+    user.date_of_birth = user_data['date_of_birth']
+    user.user_name = user_data['user_name']
+    user.password = user_data['password']
+
+    # Commit the changes to the database.
+    session.commit()
+
+    # Return a success message.
+    return jsonify({'message': 'User updated successfully'}), 200
+
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    # Get the user with the specified ID from the database.
+    with db.session as session:
+        user = session.query(User).get(user_id)
+
+    # Delete the user from the database.
+    session.delete(user)
+    session.commit()
+
+    # Return a success message.
+    return jsonify({'message': 'User deleted successfully'}), 200
+
+
+@app.route('/orders', methods=['GET'])
+def get_orders():
+    # Get all orders from the database.
+    with db.session as session:
+        orders = session.query(Order).all()
+
+    # Return a JSON response with the orders.
+    return jsonify([order.serialize() for order in orders]), 200
+
+@app.route('/orders/<int:order_id>', methods=['GET'])
+def get_order(order_id):
+    # Get the order with the specified ID from the database.
+    with db.session as session:
+        order = session.query(Order).get(order_id)
+
+    # Return a JSON response with the order.
+    return jsonify(order.serialize()), 200
+
+@app.route('/orders', methods=['POST'])
+def create_order():
+    # Get the order data from the request body.
+    order_data = request.json
+
+    # Create a new order object.
+    order = Order(
+        table_number=order_data['table_number'],
+        number_of_people=order_data['number_of_people'],
+    )
+
+    # Add the order to the database.
+    with db.session as session:
+        session.add(order)
+        session.commit()
+
+    # Return a success message.
+    return jsonify({'message': 'Order created successfully'}), 201
+
+@app.route('/orders/<int:order_id>', methods=['PUT'])
+def update_order(order_id):
+    # Get the order data from the request body.
+    order_data = request.json
+
+    # Get the order with the specified ID from the database.
+    with db.session as session:
+        order = session.query(Order).get(order_id)
+
+    # Update the order's data.
+    order.table_number = order_data['table_number']
+    order.number_of_people = order_data['number_of_people']
+
+    # Commit the changes to the database.
+    session.commit()
+
+    # Return a success message.
+    return jsonify({'message': 'Order updated successfully'}), 200
+
+
+#Orders    
+
+@app.route('/orders/<int:order_id>', methods=['DELETE'])
+def delete_order(order_id):
+    # Get the order with the specified ID from the database.
+    with db.session as session:
+        order = session.query(Order).get(order_id)
+
+    # Delete the order from the database.
+    session.delete(order)
+    session.commit()
+
+    # Return a success message.
+    return jsonify({'message': 'Order deleted successfully'}), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,9 +4,11 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(120))
+    role = db.Column(db.String(120))
+    date_of_birth = db.Column(db.Date)
+    user_name = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -14,6 +16,103 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "name": self.name,
+            "role": self.role,
+            "date_of_birth": self.date_of_birth,
+            "user_name": self.user_name,
             # do not serialize the password, its a security breach
+        }
+
+class Change(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime)
+    user_name = db.Column(db.String(120))
+    change_type = db.Column(db.String(120))
+    change_data = db.Column(db.Text)
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    table_number = db.Column(db.Integer)
+    number_of_people = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return '<Order %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "table_number": self.table_number,
+            "number_of_people": self.number_of_people,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+        }
+
+
+class FoodItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    price = db.Column(db.Float)
+    quantity = db.Column(db.Integer)
+    status = db.Column(db.String(120))
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return '<FoodItem %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "price": self.price,
+            "quantity": self.quantity,
+            "status": self.status,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+        }
+
+
+class StoreItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    price = db.Column(db.Float)
+    quantity = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return '<StoreItem %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "price": self.price,
+            "quantity": self.quantity,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+        }
+
+
+class AquaticItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    price = db.Column(db.Float)
+    quantity = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return '<AquaticItem %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "price": self.price,
+            "quantity": self.quantity,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
         }
