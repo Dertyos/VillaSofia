1b2ee8a8b0638edc361980de3a0a3de1d6c73608
Dertyos <juliansalcedo4@gmail.com>
2023-05-17 17:15:08 +0000

    Funcionando, queda pendiente, pruba de encpoints con postman

--- src/app.py
+++ src/app.py
@@ -39,8 +39,7 @@ def sitemap():
 @app.route('/users', methods=['GET'])
 def get_users():
     # Get all users from the database.
-    with db.session as session:
-        users = session.query(User).all()
+    users = User.query.all()
 
     # Return a JSON response with the users.
     return jsonify([user.serialize() for user in users]), 200
@@ -53,26 +52,22 @@ def register_change():
     change_data = request.json['change_data']
 
     # Insert a new row into the `changes` table.
-    with db.session as session:
-        change = Change(
-            timestamp=datetime.now(),
-            user_name=user_name,
-            change_type=change_type,
-            change_data=change_data,
-        )
-        session.add(change)
-        session.commit()
+    change = Change(
+        timestamp=datetime.now(),
+        user_name=user_name,
+        change_type=change_type,
+        change_data=change_data,
+    )
+    db.session.add(change)
+    db.session.commit()
 
     # Return a success message.
     return jsonify({'message': 'Change registered successfully'}), 201
 
-
-
 @app.route('/users/<int:user_id>', methods=['GET'])
 def get_user(user_id):
     # Get the user with the specified ID from the database.
-    with db.session as session:
-        user = session.query(User).get(user_id)
+    user = User.query.get(user_id)
 
     # Return a JSON response with the user.
     return jsonify(user.serialize()), 200
@@ -92,9 +87,8 @@ def create_user():
     )
 
     # Add the user to the database.
-    with db.session as session:
-        session.add(user)
-        session.commit()
+    db.session.add(user)
+    db.session.commit()
 
     # Return a success message.
     return jsonify({'message': 'User created successfully'}), 201
@@ -105,8 +99,7 @@ def update_user(user_id):
     user_data = request.json
 
     # Get the user with the specified ID from the database.
-    with db.session as session:
-        user = session.query(User).get(user_id)
+    user = User.query.get(user_id)
 
     # Update the user's data.
     user.name = user_data['name']
@@ -116,7 +109,7 @@ def update_user(user_id):
     user.password = user_data['password']
 
     # Commit the changes to the database.
-    session.commit()
+    db.session.commit()
 
     # Return a success message.
     return jsonify({'message': 'User updated successfully'}), 200
@@ -124,22 +117,21 @@ def update_user(user_id):
 @app.route('/users/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
     # Get the user with the specified ID from the database.
-    with db.session as session:
-        user = session.query(User).get(user_id)
+    user = User.query.get(user_id)
 
     # Delete the user from the database.
-    session.delete(user)
-    session.commit()
+    db.session.delete(user)
+    db.session.commit()
 
     # Return a success message.
     return jsonify({'message': 'User deleted successfully'}), 200
 
 
+
 @app.route('/orders', methods=['GET'])
 def get_orders():
     # Get all orders from the database.
-    with db.session as session:
-        orders = session.query(Order).all()
+    orders = db.session.query(Order).all()
 
     # Return a JSON response with the orders.
     return jsonify([order.serialize() for order in orders]), 200
@@ -147,8 +139,7 @@ def get_orders():
 @app.route('/orders/<int:order_id>', methods=['GET'])
 def get_order(order_id):
     # Get the order with the specified ID from the database.
-    with db.session as session:
-        order = session.query(Order).get(order_id)
+    order = db.session.query(Order).get(order_id)
 
     # Return a JSON response with the order.
     return jsonify(order.serialize()), 200
@@ -165,9 +156,8 @@ def create_order():
     )
 
     # Add the order to the database.
-    with db.session as session:
-        session.add(order)
-        session.commit()
+    db.session.add(order)
+    db.session.commit()
 
     # Return a success message.
     return jsonify({'message': 'Order created successfully'}), 201
@@ -178,59 +168,29 @@ def update_order(order_id):
     order_data = request.json
 
     # Get the order with the specified ID from the database.
-    with db.session as session:
-        order = session.query(Order).get(order_id)
+    order = db.session.query(Order).get(order_id)
 
     # Update the order's data.
     order.table_number = order_data['table_number']
     order.number_of_people = order_data['number_of_people']
 
     # Commit the changes to the database.
-    session.commit()
+    db.session.commit()
 
     # Return a success message.
     return jsonify({'message': 'Order updated successfully'}), 200
 
-
-# Order routes
-@app.route('/orders', methods=['GET'])
-def get_orders():
-    with db.session as session:
-        orders = session.query(Order).all()
-    return jsonify([order.serialize() for order in orders]), 200
-
-@app.route('/orders', methods=['POST'])
-def create_order():
-    order_data = request.json
-    order = Order(table_number=order_data['table_number'], number_of_people=order_data['number_of_people'])
-    with db.session as session:
-        session.add(order)
-        session.commit()
-    return jsonify({'message': 'Order created successfully'}), 201
-
-@app.route('/orders/<int:order_id>', methods=['PUT'])
-def update_order(order_id):
-    order_data = request.json
-    with db.session as session:
-        order = session.query(Order).get(order_id)
-        order.table_number = order_data['table_number']
-        order.number_of_people = order_data['number_of_people']
-        session.commit()
-    return jsonify({'message': 'Order updated successfully'}), 200
-
 @app.route('/orders/<int:order_id>', methods=['DELETE'])
 def delete_order(order_id):
-    with db.session as session:
-        order = session.query(Order).get(order_id)
-        session.delete(order)
-        session.commit()
+    order = db.session.query(Order).get(order_id)
+    db.session.delete(order)
+    db.session.commit()
     return jsonify({'message': 'Order deleted successfully'}), 200
 
 # OrderItem routes
 @app.route('/orderitems', methods=['GET'])
 def get_order_items():
-    with db.session as session:
-        order_items = session.query(OrderItem).all()
+    order_items = db.session.query(OrderItem).all()
     return jsonify([item.serialize() for item in order_items]), 200
 
 @app.route('/orderitems', methods=['POST'])
@@ -242,36 +202,32 @@ def create_order_item():
         item_id=order_item_data['item_id'],
         quantity=order_item_data['quantity']
     )
-    with db.session as session:
-        session.add(order_item)
-        session.commit()
+    db.session.add(order_item)
+    db.session.commit()
     return jsonify({'message': 'Order item created successfully'}), 201
 
 @app.route('/orderitems/<int:order_item_id>', methods=['PUT'])
 def update_order_item(order_item_id):
     order_item_data = request.json
-    with db.session as session:
-        order_item = session.query(OrderItem).get(order_item_id)
-        order_item.order_id = order_item_data['order_id']
-        order_item.item_type = order_item_data['item_type']
-        order_item.item_id = order_item_data['item_id']
-        order_item.quantity = order_item_data['quantity']
-        session.commit()
+    order_item = db.session.query(OrderItem).get(order_item_id)
+    order_item.order_id = order_item_data['order_id']
+    order_item.item_type = order_item_data['item_type']
+    order_item.item_id = order_item_data['item_id']
+    order_item.quantity = order_item_data['quantity']
+    db.session.commit()
     return jsonify({'message': 'Order item updated successfully'}), 200
 
 @app.route('/orderitems/<int:order_item_id>', methods=['DELETE'])
 def delete_order_item(order_item_id):
-    with db.session as session:
-        order_item = session.query(OrderItem).get(order_item_id)
-        session.delete(order_item)
-        session.commit()
+    order_item = db.session.query(OrderItem).get(order_item_id)
+    db.session.delete(order_item)
+    db.session.commit()
     return jsonify({'message': 'Order item deleted successfully'}), 200
 
 # FoodItem routes
 @app.route('/fooditems', methods=['GET'])
 def get_food_items():
-    with db.session as session:
-        food_items = session.query(FoodItem).all()
+    food_items = db.session.query(FoodItem).all()
     return jsonify([item.serialize() for item in food_items]), 200
 
 @app.route('/fooditems', methods=['POST'])
@@ -283,36 +239,32 @@ def create_food_item():
         quantity=food_item_data['quantity'],
         status=food_item_data['status'],
     )
-    with db.session as session:
-        session.add(food_item)
-        session.commit()
+    db.session.add(food_item)
+    db.session.commit()
     return jsonify({'message': 'FoodItem created successfully'}), 201
 
 @app.route('/fooditems/<int:food_item_id>', methods=['PUT'])
 def update_food_item(food_item_id):
     food_item_data = request.json
-    with db.session as session:
-        food_item = session.query(FoodItem).get(food_item_id)
-        food_item.name = food_item_data['name']
-        food_item.price = food_item_data['price']
-        food_item.quantity = food_item_data['quantity']
-        food_item.status = food_item_data['status']
-        session.commit()
+    food_item = db.session.query(FoodItem).get(food_item_id)
+    food_item.name = food_item_data['name']
+    food_item.price = food_item_data['price']
+    food_item.quantity = food_item_data['quantity']
+    food_item.status = food_item_data['status']
+    db.session.commit()
     return jsonify({'message': 'FoodItem updated successfully'}), 200
 
 @app.route('/fooditems/<int:food_item_id>', methods=['DELETE'])
 def delete_food_item(food_item_id):
-    with db.session as session:
-        food_item = session.query(FoodItem).get(food_item_id)
-        session.delete(food_item)
-        session.commit()
+    food_item = db.session.query(FoodItem).get(food_item_id)
+    db.session.delete(food_item)
+    db.session.commit()
     return jsonify({'message': 'FoodItem deleted successfully'}), 200
 
 # StoreItem routes
 @app.route('/storeitems', methods=['GET'])
 def get_store_items():
-    with db.session as session:
-        store_items = session.query(StoreItem).all()
+    store_items = db.session.query(StoreItem).all()
     return jsonify([item.serialize() for item in store_items]), 200
 
 @app.route('/storeitems', methods=['POST'])
@@ -323,35 +275,31 @@ def create_store_item():
         price=store_item_data['price'],
         quantity=store_item_data['quantity']
     )
-    with db.session as session:
-        session.add(store_item)
-        session.commit()
+    db.session.add(store_item)
+    db.session.commit()
     return jsonify({'message': 'Store item created successfully'}), 201
 
 @app.route('/storeitems/<int:store_item_id>', methods=['PUT'])
 def update_store_item(store_item_id):
     store_item_data = request.json
-    with db.session as session:
-        store_item = session.query(StoreItem).get(store_item_id)
-        store_item.name = store_item_data['name']
-        store_item.price = store_item_data['price']
-        store_item.quantity = store_item_data['quantity']
-        session.commit()
+    store_item = db.session.query(StoreItem).get(store_item_id)
+    store_item.name = store_item_data['name']
+    store_item.price = store_item_data['price']
+    store_item.quantity = store_item_data['quantity']
+    db.session.commit()
     return jsonify({'message': 'Store item updated successfully'}), 200
 
 @app.route('/storeitems/<int:store_item_id>', methods=['DELETE'])
 def delete_store_item(store_item_id):
-    with db.session as session:
-        store_item = session.query(StoreItem).get(store_item_id)
-        session.delete(store_item)
-        session.commit()
+    store_item = db.session.query(StoreItem).get(store_item_id)
+    db.session.delete(store_item)
+    db.session.commit()
     return jsonify({'message': 'Store item deleted successfully'}), 200
 
 # AquaticItem routes
 @app.route('/aquaticitems', methods=['GET'])
 def get_aquatic_items():
-    with db.session as session:
-        aquatic_items = session.query(AquaticItem).all()
+    aquatic_items = db.session.query(AquaticItem).all()
     return jsonify([item.serialize() for item in aquatic_items]), 200
 
 @app.route('/aquaticitems', methods=['POST'])
@@ -362,28 +310,25 @@ def create_aquatic_item():
         price=aquatic_item_data['price'],
         quantity=aquatic_item_data['quantity']
     )
-    with db.session as session:
-        session.add(aquatic_item)
-        session.commit()
+    db.session.add(aquatic_item)
+    db.session.commit()
     return jsonify({'message': 'Aquatic item created successfully'}), 201
 
 @app.route('/aquaticitems/<int:aquatic_item_id>', methods=['PUT'])
 def update_aquatic_item(aquatic_item_id):
     aquatic_item_data = request.json
-    with db.session as session:
-        aquatic_item = session.query(AquaticItem).get(aquatic_item_id)
-        aquatic_item.name = aquatic_item_data['name']
-        aquatic_item.price = aquatic_item_data['price']
-        aquatic_item.quantity = aquatic_item_data['quantity']
-        session.commit()
+    aquatic_item = db.session.query(AquaticItem).get(aquatic_item_id)
+    aquatic_item.name = aquatic_item_data['name']
+    aquatic_item.price = aquatic_item_data['price']
+    aquatic_item.quantity = aquatic_item_data['quantity']
+    db.session.commit()
     return jsonify({'message': 'Aquatic item updated successfully'}), 200
 
 @app.route('/aquaticitems/<int:aquatic_item_id>', methods=['DELETE'])
 def delete_aquatic_item(aquatic_item_id):
-    with db.session as session:
-        aquatic_item = session.query(AquaticItem).get(aquatic_item_id)
-        session.delete(aquatic_item)
-        session.commit()
+    aquatic_item = db.session.query(AquaticItem).get(aquatic_item_id)
+    db.session.delete(aquatic_item)
+    db.session.commit()
     return jsonify({'message': 'Aquatic item deleted successfully'}), 200
 
 # this only runs if `$ python src/app.py` is executed
